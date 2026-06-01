45ed71c7e66cf8f046d558473d753b63588601fc
SchlangeGoto <kkazimir@protonmail.com>
2026-06-01 16:02:59 +0200

    initial fabric mod setup

---
+++ src/client/java/dev/kkazi/claudecraft/client/ClaudeCraftClient.java
@@ -0,0 +1,10 @@
+package dev.kkazi.claudecraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClaudeCraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/kkazi/claudecraft/ClaudeCraft.java
@@ -0,0 +1,10 @@
+package dev.kkazi.claudecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ClaudeCraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
